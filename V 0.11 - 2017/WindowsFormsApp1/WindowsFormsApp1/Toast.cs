046b367f6c21bfeade14857fbef20fbaacf6dff5
Bruno Recalde <105523720+Hiinoken@users.noreply.github.com>
2022-07-27 21:08:06 -0300

    Add files via upload

---
+++ V 0.11 - 2017/WindowsFormsApp1/WindowsFormsApp1/Toast.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Sistema_de_Resultados_Deportivos
+{
+    internal class Toast
+    {
+        private static Toast baker = null;
+        public Toast(int x, String title, String msg)
+        {
+            baker = this;
+            SelectBread(x, title, msg);
+        }
+
+        private void MatchBeginsToast(String title, String msg)
+        {
+            new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .AddInlineImage(NewImage(new Bitmap(WindowsFormsApp1.Properties.Resources.barcelona), new Bitmap(WindowsFormsApp1.Properties.Resources.realmadrid), 1, null))
+            .Show();
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                ToastClick(1);
+            };
+        }
+
+        private void MatchScoreToast(String title, String msg)
+        {
+            new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .AddInlineImage(NewImage(new Bitmap(WindowsFormsApp1.Properties.Resources.barcelona), new Bitmap(WindowsFormsApp1.Properties.Resources.realmadrid), 2, null))
+            .Show();
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                ToastClick(1);
+            };
+        }
+
+        private void MatchRoundToast(String title, String msg)
+        {
+            new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .AddInlineImage(NewImage(new Bitmap(WindowsFormsApp1.Properties.Resources.barcelona), new Bitmap(WindowsFormsApp1.Properties.Resources.realmadrid), 2, "10"))
+            .Show();
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                ToastClick(1);
+            };
+        }
+
+        private void MatchNotificationToast(String title, String msg)
+        {
+            new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .Show();
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                ToastClick(1);
+            };
+        }
+
+        private void EventToast(String title, String msg)
+        {
+            new ToastContentBuilder()
+            .AddText(title)
+            .AddText(msg)
+            .AddInlineImage(NewImage(new Bitmap(WindowsFormsApp1.Properties.Resources.barcelona), null, 4, null))
+            .Show();
+            ToastNotificationManagerCompat.OnActivated += toastArgs =>
+            {
+                ToastClick(2);
+            };
+        }
+
+        private void ToastClick(int type)
+        {
+            Principal.AlterPrincipal(type, 4, 1);
+        }
+
+        private void SelectBread(int x, String title, String msg)
+        {
+            switch (x)
+            {
+                case 0:
+                    baker.MatchBeginsToast(title, msg);
+                    break;
+                case 1:
+                    baker.MatchScoreToast(title, msg);
+                    break;
+                case 2:
+                    baker.MatchRoundToast(title, msg);
+                    break;
+                case 3:
+                    baker.MatchNotificationToast(title, msg);
+                    break;
+                case 4:
+                    baker.EventToast(title, msg);
+                    break;
+            }
+        }
+
+        private Uri NewImage(Bitmap image1, Bitmap image2, int type, String textImage)
+        {
+            /* Types 
+             * 1) Match beginnig
+             * 2) Match score */
+            Bitmap first;
+            Bitmap second;
+            switch (type)
+            {
+                case 1: //For VS
+                    Bitmap vs = new Bitmap(WindowsFormsApp1.Properties.Resources.versus);
+
+                    first = new Bitmap(image1.Width + vs.Width, 180);
+                    using (Graphics g = Graphics.FromImage(first))
+                    {
+                        g.DrawImage(image1, 0, 15);
+                        g.DrawImage(vs, image1.Width, 0);
+                    }
+
+                    second = new Bitmap(first.Width + image2.Width, 180);
+                    using (Graphics g = Graphics.FromImage(second))
+                    {
+                        g.DrawImage(first, 0, 0);
+                        g.DrawImage(image2, first.Width, 15);
+                    }
+
+                    second.Save("toast.png");
+                    break;
+                case 2: //For score
+                    Bitmap score = new Bitmap(GetText(textImage, 1));
+
+                    first = new Bitmap(image1.Width + score.Width, 180);
+                    using (Graphics g = Graphics.FromImage(first))
+                    {
+                        g.DrawImage(image1, 0, 15);
+                        g.DrawImage(score, image1.Width, 90 - score.Height/2);
+                    }
+
+                    second = new Bitmap(first.Width + image2.Width, 180);
+                    using (Graphics g = Graphics.FromImage(second))
+                    {
+                        g.DrawImage(first, 0, 0);
+                        g.DrawImage(image2, first.Width, 15);
+                    }
+
+                    second.Save("toast.png");
+                    break;
+                case 3: //For round
+
+                    Bitmap r = new Bitmap(GetText("Round", 2));
+                    Bitmap nRound = new Bitmap(GetText(textImage, 2));
+                    Bitmap round = new Bitmap(r.Width, r.Height + nRound.Height);
+                    using (Graphics g = Graphics.FromImage(round))
+                    {
+                        g.DrawImage(r, 0, 0);
+                        g.DrawImage(nRound, -5, r.Height);
+                    }
+
+                    first = new Bitmap(image1.Width + round.Width, 180);
+                    using (Graphics g = Graphics.FromImage(first))
+                    {
+                        g.DrawImage(image1, 0, 15);
+                        g.DrawImage(round, image1.Width, 90 - round.Height / 2);
+                    }
+
+                    second = new Bitmap(first.Width + image2.Width, 180);
+                    using (Graphics g = Graphics.FromImage(second))
+                    {
+                        g.DrawImage(first, 0, 0);
+                        g.DrawImage(image2, first.Width, 15);
+                    }
+
+                    second.Save("toast.png");
+                    break;
+                case 4: //For single images
+
+                    first = new Bitmap(364, 180);
+                    using (Graphics g = Graphics.FromImage(first))
+                    {
+                        g.DrawImage(image1, 107, 15);
+                    }
+
+                    first.Save("toast.png");
+                    break;
+            }
+            Uri uri = new Uri(@"C:\Users\Hinoken\source\repos\Proyecto_ESI_2022_Certus\Sistema de Resultados Deportivos\bin\Debug\net6.0-windows10.0.17763.0\toast.png");
+            return uri;
+        }
+
+        private Bitmap GetText(String text, int size)
+        {
+            Bitmap s = null;
+            Font drawFont = null;
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            StringFormat drawFormat = new StringFormat();
+            switch (size)
+            {
+                case 1:
+                    drawFont = new Font("Segoe UI", 30);
+
+                    s = new Bitmap(text.Length * 20, 80);
+
+                    using (Graphics g = Graphics.FromImage(s))
+                    {
+                        g.DrawString(text, drawFont, drawBrush, 0, 0, drawFormat);
+                    }
+                    break;
+                case 2:
+                    drawFont = new Font("Segoe UI", 20);
+
+                    s = new Bitmap(180, 40);
+                    float x = (180 - text.Length * 20) / 2;
+
+                    using (Graphics g = Graphics.FromImage(s))
+                    {
+                        g.DrawString(text, drawFont, drawBrush, x, 0, drawFormat);
+                    }
+                    break;
+            }
+            return s;
+        }
+    }
+}
---
+++ V 0.11 - 2017/WindowsFormsApp1/WindowsFormsApp1/Users.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Resultados_Deportivos
+{
+    public partial class Users : Form
+    {
+        private static Users form = null;
+        bool editingName, editingEmail, editingPhone;
+        Usuario u = Program.user;
+        public Users()
+        {
+            InitializeComponent();
+            form = this;
+            panelChangePassword.Hide();
+            CargarUsuario();
+            SetIdioma();
+            SetTheme();
+        }
+
+        private void CargarUsuario() //Carga los botones de panelDepFav uno por uno
+        {
+            panelDepFav.Controls.Clear();
+            if (txtUser.Text == "")
+            {
+                txtUser.Text = u.nombreUsuario;
+                txtEmail.Text = u.email;
+                if (u.numeroTelefono != null)
+                {
+                    txtPhoneNumber.Text = u.numeroTelefono;
+                    btnAddNumber.Hide();
+                }
+                else
+                {
+                    txtPhoneNumber.Hide();
+                    btnEditPhone.Hide();
+                }
+            }
+            foreach (var f in u.deportesFavoritos)
+            {
+                Panel p1 = new Panel();
+
+                p1.Dock = DockStyle.Top;
+                p1.BorderStyle = BorderStyle.None;
+                p1.Size = new Size(265, 25);
+                p1.TabIndex = 0;
+
+                TextBox l1 = new TextBox();
+
+                l1.ReadOnly = true;
+                l1.Font = new Font("Segoe UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+                l1.Text = $"{f}";
+                l1.TextAlign = HorizontalAlignment.Center;
+                l1.Size = new Size(257, 25);
+                l1.AutoSize = false;
+                l1.BorderStyle = BorderStyle.FixedSingle;
+                l1.Location = new Point(0, 0);
+                l1.TabIndex = 3;
+
+                switch (AjustesDeUsuario.darkTheme)
+                {
+                    case false: //Tema claro
+                        l1.ForeColor = Color.Black;
+                        l1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+                        break;
+                    case true: //Tema oscuro
+                        l1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(110)))), ((int)(((byte)(223)))));
+                        l1.BackColor = Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+                        break;
+                }
+
+                this.panelDepFav.Controls.Add(p1); //Agrega los controles al panelDepFav
+                p1.Controls.Add(l1);
+            }
+        }
+        private void btnEditarEmail_Click(object sender, EventArgs e)
+        {
+            if (editingEmail == true)
+            {
+                txtEmail.ReadOnly = true;
+                editingEmail = false;
+                btnEditarEmail.Image = WindowsFormsApp1.Properties.Resources.pluma;
+            }
+            else
+            {
+                txtEmail.ReadOnly = false;
+                editingEmail = true;
+                btnEditarEmail.Image = WindowsFormsApp1.Properties.Resources.disco;
+            }
+        }
+
+        private void btnEditarNombre_Click(object sender, EventArgs e)
+        {
+            if (editingName == true)
+            {
+                txtUser.ReadOnly = true;
+                editingName = false;
+                btnEditarUsuario.Image = WindowsFormsApp1.Properties.Resources.pluma;
+            }
+            else
+            {
+                txtUser.ReadOnly = false;
+                editingName = true;
+                btnEditarUsuario.Image = WindowsFormsApp1.Properties.Resources.disco;
+            }
+        }
+
+        private void btnAddNumber_Click(object sender, EventArgs e)
+        {
+            txtPhoneNumber.ReadOnly = false;
+            editingPhone = true;
+            btnEditPhone.Image = WindowsFormsApp1.Properties.Resources.disco;
+            txtPhoneNumber.Show();
+            btnEditPhone.Show();
+            btnAddNumber.Hide();
+        }
+
+        private void btnChangePass_Click(object sender, EventArgs e)
+        {
+            panelChangePassword.Show();
+        }
+
+        private void btnEditPhone_Click(object sender, EventArgs e)
+        {
+            if (txtPhoneNumber.Text == "" || txtPhoneNumber.Text == u.numeroTelefono)
+            {
+                txtPhoneNumber.ReadOnly = true;
+                editingPhone = false;
+                btnEditPhone.Image = WindowsFormsApp1.Properties.Resources.pluma;
+                txtPhoneNumber.Hide();
+                btnEditPhone.Hide();
+                btnAddNumber.Show();
+            }
+            else if (editingPhone == true)
+            {
+                txtPhoneNumber.ReadOnly = true;
+                editingPhone = false;
+                btnEditPhone.Image = WindowsFormsApp1.Properties.Resources.pluma;
+            } else
+            {
+                txtPhoneNumber.ReadOnly = false;
+                editingPhone = true;
+                btnEditPhone.Image = WindowsFormsApp1.Properties.Resources.disco;
+            }
+        }
+
+        private void txtPhoneNumber_KeyPress(object sender, KeyPressEventArgs e) //Se encarga de que txtPhoneNumber solo acepte números
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+            txtRepeatPassword.Text = "";
+            panelChangePassword.Hide();
+        }
+
+        private void btnPremium_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void AlterUsers(int y)
+        {
+            switch (y)
+            {
+                case 1:
+                    if (form != null)
+                    {
+                        form.SetTheme();
+                        form.CargarUsuario();
+                    }
+                    break;
+                case 2:
+                    if (form != null)
+                    {
+                        form.SetIdioma();
+                    }
+                    break;
+            }
+        }
+    }
+}
