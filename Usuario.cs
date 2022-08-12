3d64c9d9bec6468bb1aa953a6b1efc41fefc3218
Bruno Recalde <105523720+Hiinoken@users.noreply.github.com>
2022-08-12 20:01:37 -0300

    Delete Usuario.cs

--- Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Usuario
-{
-    public string? nombreUsuario { get; set; }
-    public string? email { get; set; }
-    public string? contrasena { get; set; }
-    public int nivelPermisos { get; set; }
-    public string? numeroTelefono { get; set; }
-    public string[] deportesFavoritos { get; set; }
-}
